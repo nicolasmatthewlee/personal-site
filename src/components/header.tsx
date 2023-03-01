8650f32e767a12c5fdb009eabbf156fdfbd2b7c9
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-02-28 18:01:49 -0800

    setup FontAwesome icons: 1. install fontawesome core, 2. install icon packages, 3. install fontawesome react component, 4. create icon with <FontAwesomeIcon icon={YOUR_SPECIFIC_ICON}/>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,13 @@
+import { faFolder } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export const Header = () => {
-  return <div className="bg-red-500">header</div>;
+  return (
+    <div className="shadow w-full flex">
+      <h1 className="flex-1">Nicolas Lee</h1>
+      <div className="flex">Portfolio</div>
+      <div>Contact</div>
+      <FontAwesomeIcon icon={faFolder}></FontAwesomeIcon>
+    </div>
+  );
 };
