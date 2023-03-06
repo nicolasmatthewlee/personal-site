4946a5cc657b8061c4b7330267d9195fa8e2d615
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-03-05 17:36:59 -0800

    adjust title at larger breakpoints

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -2,8 +2,14 @@ import PROFILE from "../assets/placeholder.jpg";
 
 export const Title = () => {
   return (
-    <div className="w-full flex flex-col px-[30px] space-y-[60px]">
-      <div>
+    <div
+      className="w-full flex flex-col px-[30px] space-y-[60px]
+    md:flex-row md:space-y-0 md:justify-end md:items-center"
+    >
+      <div
+        className="
+      md:absolute md:left-[30px] md:top-[150px]"
+      >
         <h1 className="text-5xl font-medium">Nicolas Lee</h1>
         <h1 className="text-5xl text-gray-600">Full stack developer</h1>
       </div>
